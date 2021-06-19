1e3bc8987f116d0a47c6b5d4ad13e06b44f51d35
Anthony Feeney <VigilanteNighthawk@gmail.com>
2021-06-18 22:43:43 -0400

    Moved dictionary loading code into function.

--- spell_checker.py
+++ spell_checker.py
@@ -5,25 +5,28 @@ import sys
 import timeit
 import pickle
 
+def load_dictionary(filename='usa.txt'):
+    #load the dictionary if cached, otherwise build from text file
+    with open('usa.txt','r') as words_file:
+        words = words_file.readlines()
+    random.shuffle(words)
+    try:
+        dictionary_handle = open('dictionary','rb')
+        dictionary = pickle.load(dictionary_handle)
+        dictionary_handle.close()
+    except:
+        dictionary = BKTree(levenshtein_distance)
+
+        for word in words:
+            dictionary.add_element(word.strip())
+        dictionary_handle = open('dictionary','wb')
+        pickle.dump(dictionary,dictionary_handle)
+        dictionary_handle.close()
+    return dictionary
 
 
 search_string = sys.argv[1].strip()
-#load the dictionary if cached, otherwise build from text file
-with open('usa.txt','r') as words_file:
-    words = words_file.readlines()
-random.shuffle(words)
-try:
-    dictionary_handle = open('dictionary','rb')
-    dictionary = pickle.load(dictionary_handle)
-    dictionary_handle.close()
-except:
-    dictionary = BKTree(levenshtein_distance)
-
-    for word in words:
-        dictionary.add_element(word.strip())
-    dictionary_handle = open('dictionary','wb')
-    pickle.dump(dictionary,dictionary_handle)
-    dictionary_handle.close()
+dictionary = load_dictionary()
 
 
 results = dictionary.search(search_string,4)
@@ -38,3 +41,5 @@ for i in range(0,len(results)):
     word,score = results[i]
     word = word.strip()
     print(f"{word:15}  {score} ")
+
+
