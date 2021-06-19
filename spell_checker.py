81026e6b811ba418424403a32177db94a113dafd
Anthony Feeney <VigilanteNighthawk@gmail.com>
2021-06-18 22:51:03 -0400

    Modified display code to display a maximum of 25 results.

--- spell_checker.py
+++ spell_checker.py
@@ -27,17 +27,14 @@ def load_dictionary(filename='usa.txt'):
 
 search_string = sys.argv[1].strip()
 dictionary = load_dictionary()
-
-
 results = dictionary.search(search_string,4)
-
-
 
 results.sort(key= lambda result_tuple: result_tuple[1])
 print(f'Search String: {search_string}')
 print(f'Number dictionary results: {len(results)}')
 print(f'{"-"*20}')
-for i in range(0,len(results)):
+num_results_to_display = min(25,len(results))
+for i in range(0,num_results_to_display):
     word,score = results[i]
     word = word.strip()
     print(f"{word:15}  {score} ")
