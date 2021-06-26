c4c216120bf4fc52eb81c5d4ed3379d5b4edeaf6
Anthony Feeney <VigilanteNighthawk@gmail.com>
2021-06-26 18:23:21 -0400

    Defined base classes for a basic mvc framework for rendering windows.

---
+++ interface_elements/base.py
@@ -0,0 +1,74 @@
+from abc import ABC
+
+class Screen(ABC):
+    '''
+        Base class for Screens.
+        Each screen will represent 
+        the entire output of the application
+        for a signle section and will
+        act as the controller for a given 
+        application function.
+        Override this this class to create new screens.
+
+    '''
+    def __init__(self,window_stack:list):
+        '''
+            Handles initialization of the Screen.
+            Takes a copy of the window stack
+            to allow class to screens to 
+            be drawn by the calling applicaton.
+            Note: Call super before attempting to 
+            register and deregister windows.
+        '''
+        self._windows = []
+        self._window_stack = window_stack
+
+    def register_window(self,window):
+        '''Add a window to be drawn.'''
+        self._windows.append(window)
+
+    def deregister_window(self,window):
+        '''Remove window from drawing.'''
+        try:
+            self._windows.remove(window)
+        except ValueError:
+            #already removed so no need to worry
+            pass 
+
+    def process_input(self):
+        '''Override this to create the controller for the screen.'''
+        pass
+    
+    def draw(self):
+        '''Called by application to draw the windows.'''
+        for window in self._windows:
+            window.draw()
+
+    def handle_resize(self):
+        '''Mark all windows to be redrawn in case terminal is resized.'''
+        for window in self._windows:
+            window.flag_for_redraw()
+
+
+class Window(ABC):
+
+    '''
+        Base class for all window elements.  
+        Provides methods for drawing and managing redrawing of 
+        windows.
+    '''
+
+    def flag_for_redraw(self):
+        ''' Will force redraw of window in event of resize.'''
+        self._draw_element = True
+
+    def draw(self):
+        '''Called by screen class to draw the element. Override _draw_element to implement acutal draw'''
+        if(self.draw_this_cycle):
+            self._draw_element()
+            self._draw_this_cycle = False
+
+
+    def _draw_element(self):
+        '''Override this method in subclass to handle window drawing'''
+        pass
