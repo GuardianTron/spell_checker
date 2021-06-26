d5aa3054221e6a4f00e1ade573caca9f539e30c3
Anthony Feeney <VigilanteNighthawk@gmail.com>
2021-06-26 18:40:53 -0400

    Refacterd ResultsWindow to be a subclass of Window

--- interface_elements/spell_elements.py
+++ interface_elements/spell_elements.py
@@ -1,12 +1,13 @@
 import curses
+from .base import Window
 
-class ResultsWindow:
+class ResultsWindow(Window):
 
     def __init__(self,height:int,width:int,y:int,x:int,color_pair:int = None):
         self._height = height
         self._width = width
         self._results = []
-        self._window = curses.newwin(height,width,y,x)
+        super().__init__(curses.newwin(height,width,y,x))
         if color_pair is not None:
             self._window.bkgd(' ',curses.color_pair(color_pair))
 
@@ -17,11 +18,10 @@ class ResultsWindow:
     @results.setter
     def results(self,results_list):
         self._results = results_list
+        self.flag_for_redraw()
 
-    def draw(self):
-        self._window.clear()
+    def _draw_element(self):
         num_lines_to_draw = self._height if len(self._results) > self._height else len(self._results)
         for i in range(0,num_lines_to_draw - 1):
             self._window.addstr(i+1,1,self._results[i])
         self._window.border()
-        self._window.refresh()
