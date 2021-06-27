dbb6bb19960eb13c6354087ef1cf7a8313be92df
Anthony Feeney <VigilanteNighthawk@gmail.com>
2021-06-26 20:56:42 -0400

    Updated Screen.process_input to take the charcter and stdscr as attributes.

--- interface_elements/base.py
+++ interface_elements/base.py
@@ -35,7 +35,7 @@ class Screen(ABC):
             #already removed so no need to worry
             pass 
 
-    def process_input(self):
+    def process_input(self,character,stdscr):
         '''Override this to create the controller for the screen.'''
         pass
     
