2eeea4b6148a1607b5e60c9c52b5abecb19ce033
Anthony Feeney <VigilanteNighthawk@gmail.com>
2021-08-24 00:21:17 -0400

    For better cohesion, handling of individual .pbk files has been migrated to it's own class.  The threaded builder will then use this class to perform file operations on generated bktrees using instances of this class.

--- dictionary_loader.py
+++ dictionary_loader.py
@@ -1,9 +1,49 @@
+from abc import ABC, abstractmethod
+from datastructures.bk_tree import BKTree
+
+class DictionaryResource(ABC):
+
+    @abstractmethod
+    def load(self):
+        pass
+
+    @abstractmethod
+    def save(self,bk_tree:BKTree):
+        pass
+
+    @abstractmethod
+    def remove(self):
+        pass
+
 import os
 import random
 from datastructures.bk_tree import BKTreeThreaded
 from edit_distance import levenshtein_distance
 import pickle
 import threading
+
+class DictionaryFileResource(DictionaryResource):
+
+    def __init__(self,filename:str):
+        self._filename = filename
+
+    def load(self):
+        with open(self._filename,'rb') as pickle_file:
+            return pickle.load(pickle_file)
+
+    def save(self,bktree:BKTree):
+        with open(self._filename,'wb') as picke_file:
+            pickle.dump(bktree,picke_file)
+
+    def remove(self):
+        if os.path.exists(self._filename):
+            os.remove(self._filename)
+
+    @property
+    def filename(self):
+        return self._filename
+
+
 
 
 class DictionaryLoader:
@@ -16,11 +56,7 @@ class DictionaryLoader:
         self._file_ext = dictionary_file_ext
         if not os.path.isdir(self._dictionary_dir):
             os.mkdir(self._dictionary_dir)
-        self._load_percentages = {}
-        self._load_percentages_lock = threading.Lock()
         
-        self._loaded_dictionaries_lock = threading.Lock()
-        self._loaded_dictionaries = {}
     
     def get_dictionary_list(self):
         contents = os.listdir(self._dictionary_dir)
@@ -28,63 +64,36 @@ class DictionaryLoader:
         #only return dictionaries
         return [dictionary for dictionary in contents if dictionary[-len(self._file_ext):] == self._file_ext]
 
-    def create_dictionary(self,filepath:str):
+    def create_dictionary_builder(self,filepath:str):
         basename = os.path.basename(filepath).split('.')[0]
         with open(filepath,'r') as dict_text:
             words = dict_text.readlines()
         #prevent recursion issues with ordered lists
-        random.shuffle(words)
-        dictionary = self._build_tree(filepath,words)
         dictionary_file_path = os.path.join(self._dictionary_dir,basename) + self._file_ext
-        with open(dictionary_file_path,'wb') as pickle_file:
-            pickle.dump(dictionary,pickle_file)
-        return dictionary
-
+        dictionary_resource = DictionaryFileResource(dictionary_file_path)
+        return DictionaryBuilder(words,dictionary_resource,daemon=True)
+    
+    
     def load_dictionary(self,dictionary_file_name:str):
         dictionary_file_path = os.path.join(self._dictionary_dir,dictionary_file_name)
-        with open(dictionary_file_path,'rb') as pickle_file:
-            return pickle.load(pickle_file)
-
-    def _build_tree(self,filename,words):
-        dictionary = BKTreeThreaded(levenshtein_distance)
-        total = len(words)
-        for i in range(0,total):
-            dictionary.add_element(words[i])
-            self._set_load_progress(i/total)
-        return dictionary
-
-
-
-    def _set_load_progress(self,filename,percent):
-        with self._load_percentages_lock:
-            self._load_percentages[filename] = percent
-
-    def get_load_progress(self,filename):
-        with self._load_percentages_lock:
-            percentage = self._load_percentages[filename]
-        return percentage
-    
-    def get_loaded_dictionary(self,filename):
-        with self._loaded_dictionaries_lock:
-            return self._loaded_dictionaries[filename]
-    
-    def _set_loaded_dictionary(self,filename,dictionary):
-        with self._loaded_dictionaries_lock:
-            self._loaded_dictionaries[filename] = dictionary
+        return DictionaryFileResource(dictionary_file_path).load()
 
     @property
     def file_ext(self):
         return self._file_ext
 
-from typing import List
+from typing import Callable, List
 
 class DictionaryBuilder(threading.Thread):
 
-    def __init__(self,words:List[str],**kwargs):
+    def __init__(self,words:List[str],dictionary_resource:DictionaryResource,**kwargs):
         self._word_list = words
+        self._dictionary_resource = dictionary_resource
         self._abort_lock = threading.Lock()
         self._progress_lock = threading.Lock()
         self._bktree_lock = threading.Lock()
+
+        self._complete_listeners:List[Callable] = []
 
 
         self._abort_early = False
@@ -101,7 +110,13 @@ class DictionaryBuilder(threading.Thread):
         with self._progress_lock:
             return self._progress_lock
 
+    @property
+    def dictionary(self):
+        with self._bktree_lock:
+            return self._bktree
+
     def _build_tree(self,words):
+        random.shuffle(words)
         dictionary = BKTreeThreaded(levenshtein_distance)
         total = len(words)
         for i in range(0,total):
@@ -117,6 +132,7 @@ class DictionaryBuilder(threading.Thread):
     def run(self):
         with self._bktree_lock:
             self._bktree = self._build_tree(self._word_list)
+        self._dictionary_resource.save(self._bktree)
 
 
 
